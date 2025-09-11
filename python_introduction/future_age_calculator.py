86afaafb1888b52c9e177b45c95273bc2632fc8b
oumar <omaroutil@gmail.com>
2025-09-11 18:26:06 +0100

    Force commit

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+current_age = int(input("Enter your current age: "))
+current_year = 2023
+future_age_2025 = current_age + (2025 - current_year)
+
+print(f"In 2025, you will be {future_age_2025} years old.")
