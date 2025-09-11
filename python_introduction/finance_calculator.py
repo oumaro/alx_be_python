8194ac418165a4dd4af1f2ab4594905c79ea20ca
oumar <omaroutil@gmail.com>
2025-09-11 18:39:48 +0100

    Force commit

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,5 +4,6 @@ savings = income - expenses
 annual_savings = savings * 12
 interest_rate = 0.05
 interest_earned = annual_savings * interest_rate
+projected_savings = annual_savings + interest_earned
 
-print(f"Projected savings after one year, with interest, is: ${annual_savings}")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}")
