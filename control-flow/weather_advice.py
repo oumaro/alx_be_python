c96f21a77648f35a5cd1d176f9366ee6bb5abbdf
oumar <omaroutil@gmail.com>
2025-09-18 12:48:03 +0100

    add logic

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,12 @@
 
 currentWeather = input("What's the weather like today? ").lower()
 
-if currentWeather == "rainy":
-    print("Don't forget your umbrella and a raincoat.")
-elif currentWeather == "sunny":
+if currentWeather == "sunny":
     print("Wear a t-shirt and sunglasses.")
+elif currentWeather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
 elif currentWeather == "cold":
     print(" Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
 
-    
